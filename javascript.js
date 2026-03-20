28776b77a81ce45536779fa02fd94118ddd4db6d
Reshmi0912 <sit22ad103@sairamtap.edu.in>
2026-03-20 18:34:08 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if(user === "bestie" && pass === "1234") {
+    document.getElementById("loginPage").classList.remove("active");
+    document.getElementById("optionPage").classList.add("active");
+  } else {
+    document.getElementById("error").innerText = "Wrong details! Try again 💔";
+  }
+}
+
+function showPhotos() {
+  document.getElementById("optionPage").classList.remove("active");
+  document.getElementById("mainPage").classList.add("active");
+}
+
+function showGifts() {
+  document.getElementById("funnyMessage").innerText = "😂 There is no gift! Because I am the world's best gift to you 💅✨";
+}
